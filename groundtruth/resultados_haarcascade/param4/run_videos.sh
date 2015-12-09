d1c451f9306c863f567697b07a2bebe58d7a5e85
Marcos Pereira <marcos.si.pereira@gmail.com>
2015-12-09 00:17:50 -0200

    Add corrected Haarcascade results

---
+++ groundtruth/resultados_haarcascade/param4/run_videos.sh
@@ -0,0 +1,17 @@
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n1.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n2.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n3.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n4.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n5.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n6.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n7.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n8.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n9.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n10.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n11.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n12.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n13.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n14.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n15.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n16.avi 
+../vehicle_detection ../../cars3.xml ../../../videos_renomeados/n17.avi 
