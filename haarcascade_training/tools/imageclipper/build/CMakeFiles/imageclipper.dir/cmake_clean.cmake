2c92ed05717b62eeeec6e6ba130f60f7eec1dcb7
Marcos Pereira <marcos.si.pereira@gmail.com>
2016-04-09 11:37:45 -0300

    Readd image clipper to fix bug

---
+++ haarcascade_training/tools/imageclipper/build/CMakeFiles/imageclipper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/imageclipper.dir/src/imageclipper.cpp.o"
+  "bin/imageclipper.pdb"
+  "bin/imageclipper"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/imageclipper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
